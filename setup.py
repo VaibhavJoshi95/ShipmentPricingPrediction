a2141d7b052d036313d49ac3bc156c655239aca8
Vaibhav Joshi <vaibhavgjoshi95@gmail.com>
2023-12-21 10:31:38 +0530

    Updated

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 from typing import List
 
-HYPEN_E_DOT ='e .'
+HYPEN_E_DOT ='-e .'
 
 def get_requirements(file_path:str)-> List[str]:
     requirements=[]
