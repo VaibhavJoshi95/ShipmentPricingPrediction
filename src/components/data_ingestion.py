ca3807f7ebe848bf8d0dd0e0500327561b77c9cb
Vaibhav Joshi <vaibhavgjoshi95@gmail.com>
2023-12-24 17:56:59 +0530

    Updated

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,12 +5,13 @@ from src.exception import CustomException
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformation
 
 @dataclass
 class DataIngestionConfig:
     train_data_path:set=os.path.join('artifact','train.csv')
-    test_data_path:str=os.join.paat('artifact','test.csv')
-    raw_data_path:str=os.join.path('artifact','raw.csv')
+    test_data_path:str=os.path.join('artifact','test.csv')
+    raw_data_path:str=os.path.join('artifact','raw.csv')
 
 class DataIngestion:
     def __init__(self):
@@ -19,7 +20,7 @@ class DataIngestion:
         logging.info('Data Ingestion Strated')
 
         try:
-            df=pd.read_csv(os.path.join('notebook/data','SCMS_Datasets.csv'))
+            df=pd.read_csv(os.path.join('notebooks/data','SCMS_Datasets.csv'))
             logging.info('Dataset read as pandas Dataframe')
 
             os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path),exist_ok=True)
@@ -45,3 +46,5 @@ class DataIngestion:
 if __name__=='__main__':
     obj=DataIngestion()
     train_data_path,test_data_path=obj.initiate_data_ingestion()
+    data_transformation = DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data_path,test_data_path)
---
+++ src/components/data_transformation.py
@@ -0,0 +1,116 @@
+import os
+import numpy as np
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OrdinalEncoder,StandardScaler
+from src.exception import CustomException
+from src.logger import logging
+import sys
+from dataclasses import dataclass
+from src.utils import save_object
+
+@dataclass
+
+class DataTransformationConfig:
+    preprocessor_obj_file_path=os.path.join('artifact','preprocessor.pkl')
+
+class DataTransformation:
+    def __init__(self):
+        self.data_tranformation_config=DataTransformationConfig()
+
+    def get_data_transformation_object(self):
+        try:
+            logging.info('Data Transformation initiated')
+
+            categorical_cols=['Country', 'Shipment Mode']
+            numerical_cols=['Qauntity_of_pack', 'Pack Price', 'Weight (Kilograms)','Product_Insurance_USD']
+
+            Country_categories=['Rwanda', 'Vietnam', "Côte dIvoire", 'Burundi', 'Mozambique','Uganda', 'Zambia', 'Namibia', 'Congo, DRC', 'Kenya', 'Zimbabwe',
+                                'Ethiopia', 'Guyana', 'Nigeria', 'Sudan', 'South Africa','Guatemala', 'Ghana', 'Haiti', 'Botswana', 'Pakistan',
+                                'Dominican Republic', 'Liberia', 'Swaziland', 'Afghanistan','Cameroon', 'Malawi', 'Lesotho', 'Senegal', 'Tanzania',
+                                'South Sudan', 'Libya', 'Mali', 'Angola', 'Benin', 'Guinea','Togo', 'Sierra Leone']
+            
+            Shipment_categories=['Air', 'Truck', 'Air Charter', 'Ocean']
+
+            logging.info('Pipeline Initiated')
+
+            #Numerical Pipeline
+
+            num_pipeline = Pipeline(
+                steps=[
+                    ('imputer',SimpleImputer(strategy='median')),
+                    ('scaler',StandardScaler())
+                ]
+            )
+
+            #categorical Piepleine
+
+            cat_pipeline=Pipeline(
+                steps=[
+                    ('imputer',SimpleImputer(strategy='most_frequent')),
+                    ('encoder',OrdinalEncoder(categories=[Country_categories,Shipment_categories])),
+                    ('scaler',StandardScaler())
+                ]
+            )
+
+            preprocessor=ColumnTransformer([
+                ('num_pipeline',num_pipeline,numerical_cols),
+                ('cat_pipeline',cat_pipeline,categorical_cols)
+            ])
+            
+            return preprocessor
+            logging.info('Pipeline Completed')
+
+
+        except Exception as e:
+            logging.info('Exception ocured in Data tRansformation method')
+            raise CustomException(e,sys)
+        
+    def initiate_data_transformation(self,train_path,test_path):
+        try:
+            #reading Train and Test data
+            train_df=pd.read_csv(train_path)
+            test_df=pd.read_csv(test_path)
+            logging.info("Read Train and Test data completed")
+            logging.info(f'Train Dataframe Head:\n{train_df.head().to_string()}')
+            logging.info(f'Test Dataframe Head:\n{test_df.head().to_string()}')
+            logging.info('Obtaining Preprocessing object')
+
+            preprocessing_obj=self.get_data_transformation_object()
+
+            target_column_name='Freight_Cost_USD'
+            drop_columns=[target_column_name]
+
+            input_feature_train_df=train_df.drop(columns=drop_columns,axis=1)
+            target_feature_train_df=train_df[target_column_name]
+            input_feature_test_df=test_df.drop(columns=target_column_name,axis=1)
+            target_feature_test_df=test_df[target_column_name]
+
+            ## Transorming using preprocessor obj
+
+            input_feature_train_arr=preprocessing_obj.fit_transform(input_feature_train_df)
+            input_feature_test_arr=preprocessing_obj.transform(input_feature_test_df)
+
+            logging.info("Applying preprocessing object on training and testing datasets")
+
+            train_arr=np.c_[input_feature_train_arr,np.array(target_feature_train_df)]
+            test_arr=np.c_[input_feature_test_arr,np.array(target_feature_test_df)]
+
+            save_object(file_path=self.data_tranformation_config.preprocessor_obj_file_path,
+                obj=preprocessing_obj)
+            logging.info('Preprocessor pickle file saved')
+
+            return(
+                train_arr,
+                test_arr,
+                self.data_tranformation_config.preprocessor_obj_file_path
+            )       
+            
+           
+
+
+        except Exception as e:
+            logging.info('Excpetion occured in the data initiate_datatransformation')
+            raise CustomException(e,sys)
