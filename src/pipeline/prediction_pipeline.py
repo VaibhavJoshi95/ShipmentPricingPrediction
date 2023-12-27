36649e541863e204c62ff51db71f32e49a62e3f6
Vaibhav Joshi <vaibhavgjoshi95@gmail.com>
2023-12-27 17:39:07 +0530

    prediction_pipeline added

--- src/pipeline/prediction_pipeline.py
+++ src/pipeline/prediction_pipeline.py
@@ -28,7 +28,30 @@ class PredictPipeline:
             raise CustomException(e,sys)
         
 class CustomData:
-    def __int__(self,Qauntity_of_pack:int,Pack Price:float, Weight(Kilograms):float,Product_Insurance_USD:float,
-                Country:str,Shipment Mode:str):
+    def __int__(self,Country:str,Shipment_Mode:str,Qauntity_of_pack:int,Pack_Price:float,Weight_Kg:int,Product_Insurance_USD:float):
+        self.Country=Country,
+        self.Shipment_Mode=Shipment_Mode,
+        self.Qauntity_of_pack=Qauntity_of_pack,
+        self.Pack_Price=Pack_Price,
+        self.Weight_Kg=Weight_Kg,
+        self.Product_Insurance_USD=Product_Insurance_USD
+
+    def get_data_as_dataframe(self):
+        try:
+            Custome_data_input_dict={
+                'Country':[self.Country],
+                'Shipment_Mode':[self.Shipment_Mode],
+                'Quantity_of_Pack':[self.Qauntity_of_pack],
+                'Pack_Price':[self.Pack_Price],
+                'Weight_Kg':[self.Weight_Kg],
+                'Product_Insurance_USD':[self.Product_Insurance_USD]
+            }
+
+            df=pd.DataFrame(Custome_data_input_dict)
+            logging.info('Data Frame Gathered')
+            return df
 
 
+        except Exception as e:
+            logging.info("Exception occured in prediction pipeline")
+            raise CustomData(e,sys)
