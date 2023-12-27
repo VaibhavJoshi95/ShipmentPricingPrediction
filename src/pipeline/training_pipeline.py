6f763c7c7593cd3455b4df046363090fbe5c5679
Vaibhav Joshi <vaibhavgjoshi95@gmail.com>
2023-12-27 12:08:02 +0530

    updated

--- src/pipeline/training_pipeline.py
+++ src/pipeline/training_pipeline.py
@@ -13,5 +13,5 @@ if __name__=='__main__':
     train_data_path,test_data_path=obj.initiate_data_ingestion()
     data_transformation=DataTransformation()
     train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data_path,test_data_path)
-    model_trainer=ModelTrainer
+    model_trainer=ModelTrainer()
     model_trainer.initiate_model_training(train_arr,test_arr)
