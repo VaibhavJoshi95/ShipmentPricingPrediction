e842689809816e63b8e1c405381aa980afcfb4ff
Vaibhav Joshi <vaibhavgjoshi95@gmail.com>
2023-12-29 11:17:12 +0530

    updated

--- application.py
+++ application.py
@@ -2,9 +2,9 @@ from flask import Flask,request,render_template,jsonify
 from src.pipeline.prediction_pipeline import CustomData,PredictPipeline
 
 
-application=Flask(__name__)
+app=Flask(__name__)
 
-app=application
+
 
 
 @app.route('/')
--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -31,10 +31,10 @@ class ModelTrainer:
                 )
 
                 param_grid={
-                'n_estimators': [25, 50],  
-                'max_depth': [None, 10, 20, 30],  
-                'min_samples_split': [2, 5, 10],
-                'min_samples_leaf': [1, 2, 4]
+                'n_estimators': [100],  
+                'max_depth': [30],  
+                'min_samples_split': [2],
+                'min_samples_leaf': [1]
             }
                 rf_regressor=RandomForestRegressor()
 
